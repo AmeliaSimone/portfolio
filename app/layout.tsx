19418002bfecbdf0312f82ac24da1bb49b5f9a7e
Amelia <amelia.feliu06@gmail.com>
2025-01-07 13:09:58 -0300

    Fuentes2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,19 @@
 import type { Metadata } from 'next'
-import { Montserrat } from 'next/font/google'
 import '../styles/globals.scss'
-import { LanguageProvider } from '../context/language-context'
-import { Header } from '../components/header/header'
+import { Montserrat, Indie_Flower } from 'next/font/google'
 
-const inter = Montserrat({ subsets: ['latin'] })
+const montserrat = Montserrat({ 
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-montserrat',
+})
+
+const indieFlower = Indie_Flower({ 
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-indie-flower',
+})
 
 export const metadata: Metadata = {
   title: 'Portfolio Amelia Feliu',
@@ -17,15 +26,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="es">
-      <body className={inter.className}>
-        <LanguageProvider>
-          <main className="main-content">
-            <Header />
-            {children}
-          </main>
-        </LanguageProvider>
-      </body>
+    <html lang="es" className={`${montserrat.variable} ${indieFlower.variable}`}>
+      <body className={montserrat.className}>{children}</body>
     </html>
   )
 }
